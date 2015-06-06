00e630dcada6e0ebca645a6a59f81e0bb0b8ddf8
Iván Meléndez <argordmel@yahoo.es>
2015-06-05 21:11:07 -0500

    Update Core

--- core/libs/filter/filter.php
+++ core/libs/filter/filter.php
@@ -20,7 +20,7 @@
 /**
  * @see FilterInterface
  * */
-require_once CORE_PATH.'libs/filter/filter_interface.php';
+require_once __DIR__.'/filter_interface.php';
 
 /**
  * Implementación de Filtros para Kumbia
@@ -188,7 +188,7 @@ class Filter {
 	protected static function _load_filter($filter) {
 		$file = APP_PATH."extensions/filters/{$filter}_filter.php";
 		if (!is_file($file)) {
-			$file = CORE_PATH."libs/filter/base_filter/{$filter}_filter.php";
+			$file = __DIR__."/base_filter/{$filter}_filter.php";
 			if (!is_file($file)) {
 				throw new KumbiaException("Filtro $filter no encontrado");
 			}
